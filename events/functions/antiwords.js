b95b1135279889f6cd66dacf7d666609139e0d03
Sebastian Wernicke <megges14@gmail.com>
2022-10-22 20:51:06 +0200

    updated antiword

--- events/functions/antiwords.js
+++ events/functions/antiwords.js
@@ -1,21 +1,38 @@
-const antiwordsData = require('../../database/guildData/antiwords')
-  module.exports = async (message) => {
-    const antiwords = await antiwordsData.findOne({
-      GuildID: message.guild.id,
-    })
-    if (antiwords) {
-      if (message.content.match("bitch") || message.content.match("hoe") || message.content.match("slut") || message.content.match("nigga") || message.content.match("nigg") || message.content.match("dick") || message.content.match("cunt") || message.content.match("shit") || message.content.match("fuck")) {
-        message.delete();
-        message.reply("**No Bad Words Allowed Please Stop!**").then(msg => {
-          let time = '4s'
-          setTimeout(function () {
-            msg.delete();
-          }, ms(time));
-        })
-      } else {
-        return;
+const antiwordsData = require("../../database/guildData/antiwords");
+const config = require("../../words.json");
+module.exports = async (message) => {
+  const antiwords = await antiwordsData.findOne({
+    GuildID: message.guild.id,
+  });
+  const ignoremods = await ignoremodsSystem.findOne({
+    GuildID: message.guild.id,
+  });
+  if (ignoremods) {
+    if (message.member.permissions.has("MANAGE_MESSAGES")) {
+      return;
+    } else if (antiwords) {
+      if (
+        config.filter.some((word) =>
+          message.content.toLowerCase().includes(word)
+        )
+      ) {
+        if (message.deletable)
+          try {
+            await message.delete();
+            message
+              .reply("**No Bad Words Allowed Please Stop!**")
+              .then((msg) => {
+                let time = "4s";
+                setTimeout(function () {
+                  msg.delete();
+                }, ms(time));
+              });
+          } catch (error) {}
       }
-    } else if (!antiwords) {
+    } else {
       return;
     }
+  } else if (!antiwords) {
+    return;
   }
+};
