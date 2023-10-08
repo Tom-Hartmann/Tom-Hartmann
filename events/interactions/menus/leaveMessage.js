b9337dc16ba7bb37d740abd0988ee0cc2b66f66b
Sebastian Wernicke <35658558+Tom-Hartmann@users.noreply.github.com>
2023-10-08 14:32:14 +0200

    Update leaveMessage.js
    updated with images

--- events/interactions/menus/leaveMessage.js
+++ events/interactions/menus/leaveMessage.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+const path = require("path");
 const GoodbyeMsg = require("../../../database/guildData/leavemessage");
 
 module.exports = async (interaction, client) => {
@@ -7,6 +10,7 @@ module.exports = async (interaction, client) => {
 
   if (interaction.values[0] === "leave_message") {
     await interaction.deferUpdate();
+
     let data;
     try {
       data = await GoodbyeMsg.findOne({
@@ -18,6 +22,7 @@ module.exports = async (interaction, client) => {
       await msg.edit(
         "Please send the **MESSAGE** to be setup as Goodbye Message!"
       );
+
       const filter = (m) => m.author.id === interaction.member.id;
 
       const collector = await interaction.channel.createMessageCollector({
@@ -27,6 +32,29 @@ module.exports = async (interaction, client) => {
 
       collector.on("collect", async (collected) => {
         let goodbyeMsg = collected.content;
+
+        // Save the attached image, if there's any
+        if (collected.attachments.first()) {
+          let imageUrl = collected.attachments.first().url;
+
+          let response = await fetch(imageUrl);
+          let buffer = await response.buffer();
+
+          let imgName = `goodbye_${interaction.guild.id}.png`; // Using .png format
+
+          // Get the path to the base folder
+          let baseDir = path.resolve(__dirname, "..", "..", "..", "..");
+          let imgFolderPath = path.join(baseDir, "Images");
+
+          // Check if folder exists; if not, create it
+          if (!fs.existsSync(imgFolderPath)) {
+            fs.mkdirSync(imgFolderPath);
+          }
+
+          let imgPath = path.join(imgFolderPath, imgName);
+
+          fs.writeFileSync(imgPath, buffer);
+        }
 
         let newData = new GoodbyeMsg({
           ByeMsg: goodbyeMsg,
