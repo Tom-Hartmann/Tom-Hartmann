ea6da08ca368a0fd79800a9b874cba3d88ead590
Sebastian Wernicke <35658558+Tom-Hartmann@users.noreply.github.com>
2023-10-08 14:40:27 +0200

    Update guildMemberRemove.js
    updated to get the correct image for leaving

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -6,82 +6,61 @@ const path = require("path");
 const fs = require("fs");
 
 module.exports = async (member) => {
-  let data;
-  let goodbyeData;
-  let byeMsgData;
+  let data, goodbyeData, byeMsgData;
 
   try {
-    data = await memberData.findOne({
-      GuildID: member.guild.id,
-    });
+    data = await memberData.findOne({ GuildID: member.guild.id });
+    goodbyeData = await leaveChannelData.findOne({ GuildID: member.guild.id });
+    byeMsgData = await leaveMsgData.findOne({ GuildID: member.guild.id });
   } catch (error) {
-    console.error(error);
-  }
-
-  try {
-    goodbyeData = await leaveChannelData.findOne({
-      GuildID: member.guild.id,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-
-  try {
-    byeMsgData = await leaveMsgData.findOne({
-      GuildID: member.guild.id,
-    });
-  } catch (error) {
-    console.error(error);
+    console.error("Error fetching database data:", error);
   }
 
   let channelToSend = goodbyeData ? goodbyeData.Bye : data.ChannelID;
-  if (!channelToSend) return;
-
   let customMessage = byeMsgData ? byeMsgData.ByeMsg : null;
-  if (customMessage) {
-    customMessage = customMessage.replace("{user.mention}", member.toString());
-    customMessage = customMessage.replace("{user.name}", member.user.tag);
-    customMessage = customMessage.replace("{server}", member.guild.name);
-    customMessage = customMessage.replace(
-      "{membercount}",
-      member.guild.memberCount.toString()
-    );
-  }
 
-  let imagePath = path.join(
+  try {
+    let channel = member.guild.channels.cache.get(channelToSend);
+
+    if (customMessage) {
+      sendCustomMessage(channel, member, customMessage);
+    } else {
+      channel.send({ embeds: [createDefaultEmbed(member)] });
+    }
+  } catch (error) {
+    console.error("Error sending member left notification:", error);
+  }
+};
+
+function sendCustomMessage(channel, member, customMessage) {
+  let replacedMessage = customMessage
+    .replace(/{user.mention}/g, member.user.toString())
+    .replace(/{user.name}/g, member.user.tag)
+    .replace(/{server}/g, member.guild.name)
+    .replace(/{membercount}/g, member.guild.memberCount.toString());
+
+  const imagePath = path.join(
     __dirname,
     "..",
     "..",
     "Images",
-    `${member.guild.id}.png`
+    `goodbye_${member.guild.id}.png`
   );
-  let imageUrl = fs.existsSync(imagePath)
-    ? `attachment://${member.guild.id}.png`
-    : null;
 
-  if (customMessage && imageUrl) {
-    member.guild.channels.cache.get(channelToSend).send({
-      content: customMessage,
-      files: [imagePath],
-    });
-  } else if (customMessage) {
-    member.guild.channels.cache.get(channelToSend).send(customMessage);
+  if (fs.existsSync(imagePath)) {
+    channel.send(replacedMessage, { files: [imagePath] });
   } else {
-    const embed = new EmbedBuilder()
-      .setTitle("Member Left")
-      .setDescription(
-        `User: ${member.user.tag} (${member})\nUser ID: ${member.id}\nAcc. Created: ${member.user.createdAt}\nServer Member Count: ${member.guild.memberCount}`
-      )
-      .setColor("Green")
-      .setTimestamp()
-      .setThumbnail(`${member.user.avatarURL()}`);
-
-    if (imageUrl) embed.attachFiles([imagePath]);
-
-    try {
-      member.guild.channels.cache.get(channelToSend).send({ embeds: [embed] });
-    } catch (error) {
-      console.error(error);
-    }
+    channel.send(replacedMessage);
   }
-};
+}
+
+function createDefaultEmbed(member) {
+  return new EmbedBuilder()
+    .setTitle("Member Left")
+    .setDescription(
+      `User: ${member.user.tag} (${member})\nUser ID: ${member.id}\nAcc. Created: ${member.user.createdAt}\nServer Member Count: ${member.guild.memberCount}`
+    )
+    .setColor("Red")
+    .setTimestamp()
+    .setThumbnail(`${member.user.avatarURL()}`);
+}
