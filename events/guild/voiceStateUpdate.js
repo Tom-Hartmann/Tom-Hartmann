bc27abf9c2a03ec88ac8069bada74b3aeb5020da
Sebastian Wernicke <35658558+Tom-Hartmann@users.noreply.github.com>
2023-10-08 16:42:47 +0200

    Update voiceStateUpdate.js

--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -13,16 +13,28 @@ module.exports = async (oldState, newState) => {
     return;
   }
 
-  if (!data) return;
+  if (!data || !data.TemplateChannelID) return;
 
-  // User joined the pre-defined channel to create a new private channel
-  if (newState.channelId && newState.channelId === data.ChannelID) {
+  // Fetch or create the category.
+  let category = newState.guild.channels.cache.find(
+    (c) => c.name == "✨Private Channels✨" && c.type == "GUILD_CATEGORY"
+  );
+
+  if (!category) {
+    category = await newState.guild.channels.create("✨Private Channels✨", {
+      type: "GUILD_CATEGORY",
+      position: newState.guild.channels.cache.size, // Should create it at the bottom.
+    });
+  }
+
+  // User joined the pre-defined channel to create a new private channel.
+  if (newState.channelId && newState.channelId === data.TemplateChannelID) {
     try {
       const newVoiceChannel = await newState.guild.channels.create(
         `Private-${newState.member.displayName}`,
         {
           type: "GUILD_VOICE",
-          parent: data.CategoryID,
+          parent: category.id,
           reason: "Private voice channel creation",
           permissionOverwrites: [
             {
@@ -61,9 +73,9 @@ module.exports = async (oldState, newState) => {
         }
       );
 
-      await newState.setChannel(newVoiceChannel); // Move the member to the new private channel
+      await newState.setChannel(newVoiceChannel); // Move the member to the new private channel.
     } catch (error) {
-      const logChannel = guild.channels.cache.get(ERROR_LOGS_CHANNEL);
+      const logChannel = newState.guild.channels.cache.get(ERROR_LOGS_CHANNEL);
       if (logChannel && logChannel.isText()) {
         logChannel.send("Error creating private voice channel:", error);
       }
@@ -71,7 +83,7 @@ module.exports = async (oldState, newState) => {
     }
   }
 
-  // Check if a user left a channel and if it's a private channel to delete when empty
+  // Check if a user left a channel and if it's a private channel to delete when empty.
   if (oldState.channelId && !newState.channelId) {
     const voiceChannel = oldState.channel;
     if (
@@ -79,12 +91,20 @@ module.exports = async (oldState, newState) => {
       voiceChannel.members.size === 0
     ) {
       voiceChannel.delete("Private voice channel empty").catch((error) => {
-        const logChannel = guild.channels.cache.get(ERROR_LOGS_CHANNEL);
+        const logChannel =
+          newState.guild.channels.cache.get(ERROR_LOGS_CHANNEL);
         if (logChannel && logChannel.isText()) {
           logChannel.send("Error deleting private voice channel:", error);
         }
         console.error("Error deleting private voice channel:", error);
       });
+
+      // If category is empty after deleting the voice channel, delete the category.
+      if (category.children.size === 1) {
+        category.delete("No channels in category").catch((error) => {
+          console.error("Error deleting category:", error);
+        });
+      }
     }
   }
 };
