f22bb42725e862c25a137f81787a0f8436c3c547
Sebastian Wernicke <35658558+Tom-Hartmann@users.noreply.github.com>
2023-10-08 12:42:08 +0200

    Update guildMemberRemove.js
    did the same here

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,26 +1,87 @@
 const memberData = require("../../database/guildData/memberupdates");
+const leaveChannelData = require("../../database/guildData/leavechannel");
+const leaveMsgData = require("../../database/guildData/leavemessage");
 const { EmbedBuilder } = require("discord.js");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = async (member) => {
   let data;
+  let goodbyeData;
+  let byeMsgData;
+
   try {
     data = await memberData.findOne({
-      GuildID: channel.guild.id,
+      GuildID: member.guild.id,
     });
-  } catch (error) {}
-
-  if (!data) return;
-
-  const embed = new EmbedBuilder()
-    .setTitle("Member Left")
-    .setDescription(
-      `User: ${member.user.tag} (${member})\nUser ID: ${member.id}\nAcc. Created: ${member.user.createdAt}\nServer Mmebr Count: ${member.guild.memberCount}`
-    )
-    .setColor("Green")
-    .setTimestamp()
-    .setThumbnail(`${member.user.avatarURL}`);
+  } catch (error) {
+    console.error(error);
+  }
 
   try {
-    member.guild.channels.cache.get(data.ChannelID).send({ embeds: [embed] });
-  } catch (error) {}
+    goodbyeData = await leaveChannelData.findOne({
+      GuildID: member.guild.id,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    byeMsgData = await leaveMsgData.findOne({
+      GuildID: member.guild.id,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  let channelToSend = goodbyeData ? goodbyeData.Bye : data.ChannelID;
+  if (!channelToSend) return;
+
+  let customMessage = byeMsgData ? byeMsgData.ByeMsg : null;
+  if (customMessage) {
+    customMessage = customMessage.replace("{user.mention}", member.toString());
+    customMessage = customMessage.replace("{user.name}", member.user.tag);
+    customMessage = customMessage.replace("{server}", member.guild.name);
+    customMessage = customMessage.replace(
+      "{membercount}",
+      member.guild.memberCount.toString()
+    );
+  }
+
+  let imagePath = path.join(
+    __dirname,
+    "..",
+    "..",
+    "Images",
+    `${member.guild.id}.png`
+  );
+  let imageUrl = fs.existsSync(imagePath)
+    ? `attachment://${member.guild.id}.png`
+    : null;
+
+  if (customMessage && imageUrl) {
+    member.guild.channels.cache.get(channelToSend).send({
+      content: customMessage,
+      files: [imagePath],
+    });
+  } else if (customMessage) {
+    member.guild.channels.cache.get(channelToSend).send(customMessage);
+  } else {
+    const embed = new EmbedBuilder()
+      .setTitle("Member Left")
+      .setDescription(
+        `User: ${member.user.tag} (${member})\nUser ID: ${member.id}\nAcc. Created: ${member.user.createdAt}\nServer Member Count: ${member.guild.memberCount}`
+      )
+      .setColor("Green")
+      .setTimestamp()
+      .setThumbnail(`${member.user.avatarURL()}`);
+
+    if (imageUrl) embed.attachFiles([imagePath]);
+
+    try {
+      member.guild.channels.cache.get(channelToSend).send({ embeds: [embed] });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 };
