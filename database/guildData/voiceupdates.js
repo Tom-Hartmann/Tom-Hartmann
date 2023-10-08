e0e0d00dbe1f0e98aa88fce2ecd91687175f265e
Sebastian Wernicke <35658558+Tom-Hartmann@users.noreply.github.com>
2023-10-08 16:37:25 +0200

    Update voiceupdates.js
    not necessary after all

--- database/guildData/voiceupdates.js
+++ database/guildData/voiceupdates.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const voiceSchema = new mongoose.Schema({
   GuildID: String,
   TemplateChannelID: String,
-  CategoryID: String, 
 });
 const voiceModel = (module.exports = mongoose.model(
   "voiceupdates",
